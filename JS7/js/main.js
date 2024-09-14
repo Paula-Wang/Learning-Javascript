b1ace7c4a09786ef0b88c4cd96c28f138080a327
Paula-Wang <nimopaula8@gmail.com>
2024-09-14 20:47:27 +0300

    feat: :sparkles: add code for switch statements

--- JS7/js/main.js
+++ JS7/js/main.js
@@ -1,35 +1,65 @@
-// //Conditionals: If Statements
+// // //Conditionals: If Statements
 
-// //Syntax
-// if (condition) {
-//     //run some code
-// } else {
-//     //run some different code
+// // //Syntax
+// // if (condition) {
+// //     //run some code
+// // } else {
+// //     //run some different code
+// // }
+
+// //Decision tree!
+// let playerOne = "rock"; 
+// let computer = "scissors";
+
+// if (playerOne === computer) {
+//     console.log("It is a tie game!");
+// } else if (playerOne === "rock") {
+//     if (computer === "paper") {
+//         console.log(`Computer wins with ${computer}!`);
+//     } else {
+//         console.log(`Player wins with ${playerOne}!`);
+//     }
+// } else if (playerOne === "paper") {
+//     if (computer === "scissors") {
+//         console.log(`Computer wins with ${computer}!`);
+//     } else {
+//         console.log(`Player wins with ${playerOne}!`);
+//     }
+// } else if (playerOne === "scissors") {
+//     if (computer === "rock") {
+//         console.log(`Computer wins with ${computer}!`);
+//     } else {
+//         console.log(`Player wins with ${playerOne}!`);
+//     }
 // }
 
-//Decision tree!
-let playerOne = "rock"; 
-let computer = "scissors";
+//Conditionals: Switch Statements
 
-if (playerOne === computer) {
-    console.log("It is a tie game!");
-} else if (playerOne === "rock") {
-    if (computer === "paper") {
-        console.log(`Computer wins with ${computer}!`);
-    } else {
-        console.log(`Player wins with ${playerOne}!`);
-    }
-} else if (playerOne === "paper") {
-    if (computer === "scissors") {
-        console.log(`Computer wins with ${computer}!`);
-    } else {
-        console.log(`Player wins with ${playerOne}!`);
-    }
-} else if (playerOne === "scissors") {
-    if (computer === "rock") {
-        console.log(`Computer wins with ${computer}!`);
-    } else {
-        console.log(`Player wins with ${playerOne}!`);
-    }
+//syntax
+let playerOne = "scissors";
+let computer = "rock";
+
+switch (playerOne) {
+    case computer:
+        console.log("Tie game!");
+        break;
+        case "rock":
+            if( computer === "paper") {
+                console.log("computer wins!");
+            } else {
+                console.log("playerOne wins!");
+            }
+            break;
+            case "paper":
+            if( computer === "scissors") {
+                console.log("computer wins!");
+            } else {
+                console.log("playerOne wins!");
+            }
+            default:
+                if( computer === "rock") {
+                    console.log("computer wins!");
+                } else {
+                    console.log("playerOne wins!");
+                }
 }
-
